e6653950e6f39341bc9d5d25836e86e4ea397952
Andrii Maglovanyi <andrii.maglovanyi@gmail.com>
2025-02-21 16:18:17 +0000

    fix middleware conflict

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,55 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-
 import { i18n } from "./dictionaries/i18n-config";
+import { kv } from "@vercel/kv";
+
+const ADMIN_API_KEY = process.env.ADMIN_API_KEY!;
+
+interface Redirect {
+  url: string;
+  hits: number;
+}
 
 export async function middleware(request: NextRequest) {
   const url = request.nextUrl;
   const { pathname, hostname } = url;
   const { locales, defaultLocale } = i18n;
 
-  if (["admin.mandrii.com", "ref.mandrii.com"].includes(hostname)) {
+  if (hostname.startsWith("ref.")) {
+    const rawTopic = pathname.slice(1);
+
+    if (!rawTopic) {
+      return NextResponse.next();
+    }
+
+    const topic = decodeURI(rawTopic);
+
+    try {
+      const redirect = await kv.get<Redirect>(`ref:${topic}`);
+
+      if (!redirect) {
+        return NextResponse.next();
+      }
+
+      redirect.hits += 1;
+      await kv.set(`ref:${topic}`, redirect);
+
+      return NextResponse.redirect(redirect.url);
+    } catch (error) {
+      return Response.json({ error: "Internal Server Error" }, { status: 500 });
+    }
+  }
+
+  if (hostname === "admin.mandrii.com") {
     return NextResponse.next();
+  }
+
+  if (pathname.startsWith("/api/ref")) {
+    const apiKey = request.headers.get("x-api-key");
+
+    if (!apiKey || apiKey !== ADMIN_API_KEY) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
   }
 
   const pathnameHasLocale = locales.find(
@@ -29,5 +69,9 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|admin|auth|qr|images|_next|assets|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|admin|auth|qr|images|_next|assets|favicon.ico).*)", // Main middleware
+    "/api/admin/:path*", // Matches /api/admin/*
+    "/:topic*", // Matches /ref.mandrii.com/*
+  ],
 };
